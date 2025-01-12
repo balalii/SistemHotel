86f25c63de4fdd76c3c317f565c928a39f673281
Iqbal Ali <iqbalali2606@gmail.com>
2025-01-12 23:33:36 +0700

    update: serviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,27 +3,23 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-public function register()
+    public function register(): void
     {
-    if(config('app.env') === 'production') {
-            $this->app['request']->server->set('HTTPS', true); 
+        //
     }
-}
 
     /**
      * Bootstrap any application services.
      */
-public function boot()
-{
-    if(env('APP_ENV') !== 'local') {
-        URL::forceScheme('https');
+    public function boot(): void
+    {
+        //
     }
-}
 }
