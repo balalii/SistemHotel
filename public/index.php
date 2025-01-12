e095f6abda5ef3fe8af766c65bc8c992fb36dc55
Iqbal Ali <iqbalali2606@gmail.com>
2025-01-13 00:34:01 +0700

    update: index.php

--- public/index.php
+++ public/index.php
@@ -16,7 +16,4 @@ require __DIR__.'/../vendor/autoload.php';
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
 
-    // set the public path to this directory
-$app->bind('path.public', function() {
-return __DIR__;
-});
+    
